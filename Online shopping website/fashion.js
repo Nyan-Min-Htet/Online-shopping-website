bfd4b574f393a0065164e03fd2574cb754c2c387
Nyan Min Htet <110186871+Nyan-Min-Htet@users.noreply.github.com>
2024-05-05 23:23:52 +0630

    Add files via upload

---
+++ Online shopping website/fashion.js
@@ -0,0 +1,547 @@
+const data =[
+    {
+        'id':1,
+        img: 'img/shirt1.jpg',
+        title: 'DURANGO FLANNEL',
+        color: '80% cotton 20% wool',
+        para:  'Original plaid pattern in burgundy and navy',
+        made:  'Soft touch flannel fabric',
+        madeIn: 'Regular fit with shirt tail hem',
+        model: '6 foot, 195 pounds, wearing size large'
+    },
+    {
+        'id':2,
+        img: 'img/shirt2.jfif',
+        title: 'GAP Men 3-Packs Short Sleeve V-Neck Tee T-Shirt',
+        color: '100% Cotton',
+        para:  'Machine Wash',
+        made:  'Imported',
+        madeIn: 'Pull On',
+        model: '$94.00'
+    },
+    {
+        'id':3,
+        img:'img/shirt3.jfif',
+        title: 'LIGHTWEIGHT FABRIC',
+        color: '100% ORGANIC COTTON',
+        para:  'UNBLEACHED / UNDYED',
+        made:  'MADE IN TAMIL NADU INDIA',
+        madeIn: 'Compostion',
+        model: '$84.00'
+       
+    },
+    {
+        'id':4,
+        img : 'img/shirt4.webp',
+        title: 'HERRINGBONE KNIT COTTON POLO',
+        color: 'Herringbone fabric',
+        para:  'Made of 100% cotton',
+        made:  'Three-button placket fastening',
+        madeIn: 'Short sleeves',
+        model: 'Ribbed Trims'
+    },
+    {
+        'id':5,
+        img : 'img/shirt5.jfif',
+        title: 'AprilRoseandCO',
+        color: 'Loose',
+        para:  'Thigh Length',
+        made:  'Regular',
+        madeIn: 'Buttons',
+        model: 'Sleeveless'
+    },
+    {
+        'id':6,
+        img: 'img/shirt6.jfif',
+        title: 'Casual Vintage Loose Sweater',
+        color: 'Standard',
+        para:  'Polyester, Cotton, Acrylic',
+        made:  'Regular',
+        madeIn: 'O-Neck',
+        model: 'kokopiecoco'
+    },
+    {
+        'id':7,
+       img: 'img/shirt7.jfif',
+        title: 'Men Long Sleeve Shirt Pure',
+        color: 'Single Breasted',
+        para:  'Regular',
+        made:  'Turn-down Collar',
+        madeIn: 'Casual shirt',
+        model: 'EVER CHANGING'
+    }
+]
+const data1 =[
+    {
+        'id': 1,
+        img : 'img/suit1.png',
+        title1: 'ANCHOR JACKET - BLACK',
+        color1: 'Fit-Slim Fit',
+        para1:  'Colors-All',
+        made1:  'Brand-Mallik Tailors',
+        madeIn1: 'Fabric-Polyester',
+        model1: '$499'
+    },
+    {
+        'id': 2,
+        img : 'img/suit2.jfif',
+        title1: 'HERRINGBONE SUIT IN BEIGE COTTON',
+        color1: 'Do not bleach',
+        para1:  'Normal steam or dry iron on low',
+        made1:  'Gentle dry clean with perchlorethylene',
+        madeIn1: 'Do not tumble dry',
+        model1: '$700.00'
+    },
+    {
+        'id': 3,
+        img : 'img/suit3.jfif',
+        title1: 'Korean Fashion Over The Knee Min Length Leather',
+        color1: 'Autumn And Winter',
+        para1:  'Casual',
+        made1:  'Regular',
+        madeIn1: 'Conventional',
+        model1: 'MMK 111,401 <del>MMK 171,402</del><span style="color: red;margin-left: 10px;">35% off</span>'    
+    },
+    {
+        'id': 4,
+        img : 'img/suit4.webp',
+        title1: 'Regular Fit Navy Twill',
+        color1: 'Modern fit',
+        para1:  '52% wool, 43% recycled polyester, 3% elastane',
+        made1:  'Matching style sold separately',
+        madeIn1: 'Dry clean',
+        model1: 'Imported'  
+    },
+    {
+        'id': 5,
+        img : 'img/suit5.webp',
+        title1: 'Coral Suit',
+        color1: '55% wool, 35% cotton, 10% silk',
+        para1:  'Half canvas',
+        made1:  'Naturla shoulders',
+        madeIn1: 'Patch pockets',
+        model1: 'Unlined'  
+    },
+    {
+        'id': 6,
+        img : 'img/suit6.jpg',
+        title1: 'Handmade',
+        color1: 'Green',
+        para1:  'Premium Fabric',
+        made1:  'Dry Clean Only',
+        madeIn1: 'Listin Include',
+        model1: '(Coat+ Vest + Pant)'  
+    },
+    {
+        'id': 7,
+        img : 'img/suit7.jpg',
+        title1: 'Men Suit Business Casual',
+        color1: 'Blue',
+        para1:  'LYPOCS',
+        made1:  'Cotton',
+        madeIn1: 'Regular',
+        model1: 'ShirtSleeve' 
+    }
+]
+const data2 =[
+    {
+        'id': 1,
+        img : 'img/trouser1.png',
+        title2: 'Women Suit Pant ',
+        color2: 'Light Gray',
+        para2:  'Plain',
+        made2:  'Straight Leg',
+        madeIn2: 'Button, Zipper',
+        model2: 'Non-Stretch' 
+    },
+    {
+        'id': 2,
+        img : 'img/trouser2.jfif',
+        title2: 'High-rise cotton trousers',
+        color2: '100% cotton',
+        para2:  'Black',
+        made2:  'Italy',
+        madeIn2: 'Wash at 40 C',
+        model2: '$395' 
+    },
+    {
+        'id': 3,
+        img : 'img/trouser3.jfif',
+        title2: 'Crisscross Ribbon Drape Comfy Pants',
+        color2: 'Crisscross ribbon at front',
+        para2:  'Seam detailing',
+        made2:  'Welt pockets at back',
+        madeIn2: 'Not lined',
+        model2: '$42.90' 
+    },
+    {
+        'id': 4,
+        img : 'img/trouser4.jpg',
+        title2: 'Mens Bid And Tall Linen Pants',
+        color2: '100% Linen pants',
+        para2:  'Flat Front Pant',
+        made2:  'Modern Fit',
+        madeIn2: 'Salmon color',
+        model2: '$125'
+    },
+    {
+        'id': 5,
+        img : 'img/trouser5.jpg',
+        title2: 'Washable Wool KhakiTan Pant',
+        color2: 'Khaki Tan',
+        para2:  '70% super 120s Wool, 30$ Polyester',
+        made2:  '9 Oz',
+        madeIn2: 'Machine Wash Cold, Gentle cycle',
+        model2: '$129.99'
+    },
+    {
+        'id': 6,
+        img : 'img/trouser6.webp',
+        title2: 'Ribbed Regular Fit Pants',
+        color2: 'Straight fit',
+        para2:  '1 pair of pants',
+        made2:  'Machine Wash',
+        madeIn2: '76% lenzing ecoveroviscose, 22% ployester, 2% elastane',
+        model2: '$429'
+    },
+    {
+        'id': 7,
+        img : 'img/trouser7.jpg',
+        title2: 'Men Pants Straight Trousers British Style',
+        color2: 'Spring And Autumn',
+        para2:  'Wedding',
+        made2:  'Smart Casual',
+        madeIn2: 'Zipper Fly',
+        model2: 'MMK 61,667'
+    }
+]
+const data3 =[
+    {
+        'id': 1,
+        img : 'img/shoe1.jfif',
+        title3: 'Nike SB Satefan Janoski',
+        color3: 'White',
+        para3:  'Low Top',
+        made3:  'Athletic',
+        madeIn3: 'Staefan Janoski',
+        model3: '$75.00'
+    },
+    {
+        'id': 2,
+        img : 'img/shoe2.webp',
+        title3: 'OUT OF STOCK',
+    },
+    {
+        'id': 3,
+        img : 'img/shoe3.webp',
+        title3: 'ADIDAS PERFORMACNE Running Shoe',
+        color3: 'Plain Colored',
+        para3:  'Round Cap',
+        made3:  '4-hole lacing',
+        madeIn3: 'Treaded sole',
+        model3: '$209.90'
+    },
+    {
+        'id': 4,
+        img : 'img/shoe4.jpg',
+        title3: 'Converse Chuck Taylor',
+        color3: 'Athletic',
+        para3:  'New with box',
+        made3:  'Converse',
+        madeIn3: 'Sneaker',
+        model3: '$87.00'
+    },
+    {
+        'id': 5,
+        img : 'img/shoe5.webp',
+        title3: 'DESTIN LOAFER',
+        color3: 'Orange leather sole',
+        para3:  'Black leather edge',
+        made3:  'Shiny black PVD-plated buckle',
+        madeIn3: 'Black calfskin insole and lining',
+        model3: '$1,100'
+    },
+    {
+        'id': 6,
+        img : 'img/shoe6.jpg',
+        title3: 'New Balance 990v4',
+        color3: 'Rubber',
+        para3:  'Leather',
+        made3:  'Lace-up',
+        madeIn3: 'Dual density collar foam',
+        model3: '$230.00'
+    },
+    {
+        'id': 7,
+        img : 'img/shoe7.jfif',
+        title3: 'Wedding Shoes',
+        color3: 'Gold',
+        para3:  'Wedding',
+        made3:  'Leather',
+        madeIn3: 'Shipping in 24 hours',
+        model3: '$139.00'
+    }
+]
+const dataContainer= document.querySelector('.column')
+function image (){
+   let imageData= data.map((e)=>{
+        return `
+        <div class="flip-box" >
+        <div class="flip-box-inner">
+          <div class="flip-box-front">
+            <img src=${e.img} alt="Paris" style="width:300px;height:375px;border-radius:10px;">
+          </div>
+          <div class="flip-box-back">
+              <h2>${e.title}</h2>
+              <p>${e.color}</p>
+              <p>${e.para}</p>
+              <p>${e.made}</p>
+              <p>${e.madeIn}</p> 
+              <p>${e.model}</p>
+            <button type="button" style="width:50%; font-size: 17px;margin-top: 20px;">
+            <a href="detail1.html" style="text-decoration: none; color: white;">Buy Now</a></button>
+          </div>
+        </div>
+      </div>
+          `
+    });
+    imageData =imageData.join('');
+    dataContainer.innerHTML =imageData;
+}
+image();
+
+const dataContainer1= document.querySelector('.column1')
+function image1(){
+    let imageData1=data1.map((e)=>{
+        return`
+        <div class="flip-box">
+        <div class="flip-box-inner">
+          <div class="flip-box-front">
+            <img src=${e.img} alt="Paris" style="width:285px;height:375px;margin-top:10px;border-radius:10px;">
+          </div>
+          <div class="flip-box-back">
+            <h2>${e.title1}</h2>
+            <p>${e.color1}</p>
+            <p>${e.para1}</p>
+            <p>${e.made1}</p>
+            <p>${e.madeIn1}</p>
+            <p>${e.model1}</p>
+            <button type="button" style="width:50%; font-size: 17px;margin-top: 20px;">
+            <a href="detail2.html" style="text-decoration: none; color: white;">Buy Now</a></button>
+          </div>
+        </div>
+      </div>    
+        `
+    });
+    imageData1=imageData1.join('');
+    dataContainer1.innerHTML=imageData1;
+}
+image1(); 
+
+const dataContainer2=document.querySelector('.column2')
+function image2(){
+    let imageData2=data2.map((e)=>{
+        return `
+        <div class="flip-box">
+        <div class="flip-box-inner">
+          <div class="flip-box-front">
+            <img src=${e.img} alt="Paris" style="width:282px;height:375px;margin-top:10px;border-radius:10px;">
+          </div>
+          <div class="flip-box-back">
+            <h2>${e.title2}</h2>
+            <p>${e.color2}</p>
+            <p>${e.para2}</p>
+            <p>${e.made2}</p>
+            <p>${e.madeIn2}</p>
+            <p>${e.model2}</p>
+            <button type="button" style="width:50%; font-size: 17px;margin-top: 20px;">
+            <a href="detail10.html" style="text-decoration: none; color: white;">Buy Now</a></button>
+          </div>
+        </div>
+      </div> 
+        `
+    });
+    imageData2=imageData2.join('');
+    dataContainer2.innerHTML=imageData2;
+}
+image2();
+
+const dataContainer3=document.querySelector('.column3')
+function imgae3(){
+    let imageData3=data3.map((e)=>{
+        return`
+        <div class="flip-box">
+        <div class="flip-box-inner">
+          <div class="flip-box-front">
+            <img src=${e.img} alt="Paris" style="width:291px;height:375px;margin-top:10px;border-radius:10px;">
+          </div>
+          <div class="flip-box-back">
+            <h2>${e.title3}</h2>
+            <p>${e.color3}</p>
+            <p>${e.para3}</p>
+            <p>${e.made3}</p>
+            <p>${e.madeIn3}</p>
+            <p>${e.model3}</p>
+            <button type="button" style="width:50%; font-size: 17px;margin-top: 20px;">
+            <a href="detail.html" style="text-decoration: none; color: white;">Buy Now</a></button>
+          </div>
+        </div>
+      </div> 
+        `
+    });
+    imageData3=imageData3.join('');
+    dataContainer3.innerHTML=imageData3;
+}
+imgae3();
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+  });
+
+var modal = document.getElementById('id01');
+		
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+function RegisterValidate(){
+    var uemail=document.registerForm.email.value;
+    var atposition=uemail.indexOf("@");
+    var dotposition=uemail.lastIndexOf(".");
+    var upsw=document.registerForm.psw.value;
+    var ureppsw=document.registerForm.reppsw.value;
+    var x=document.registerForm.email.value;
+    if (uemail == null || uemail == "") {
+        alert("Email is required");
+        return false;
+    }
+    else if(atposition <1 || dotposition<atposition+2 || dotposition+2 >= uemail.length){
+        alert("Please enter a valid email address");
+        return false;
+    }
+    else if(upsw == null || upsw == ""){
+        alert("Password is required");
+        return false;
+    }
+    else if(upsw.length<6){
+        alert("Password is must be greater than 6");
+        return false;
+    }
+    else if(upsw == null || upsw == ""){
+        alert("Repeat Password is required");
+        return false;
+    }
+    else if (upsw != ureppsw){
+        alert("Your repeat password is must be equal to password");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function EmailValidate(){
+    var uemail=document.emailForm.email.value;
+    var atposition=uemail.indexOf("@");
+    var dotposition=uemail.lastIndexOf(".");
+     if (uemail == null || uemail == "") {
+        alert("Email is required");
+        return false;
+    }
+    else if(atposition <1 || dotposition<atposition+2 || dotposition+2 >= uemail.length){
+        alert("Please enter a valid email address");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function LoginValidate(){
+    var usname=document.loginForm.uname.value;
+    var upsw=document.loginForm.psw.value;
+    if(usname == null || usname == ""){
+        alert("Your name cannto be blank");
+        return false
+    }
+    else if(upsw == null || upsw == ""){
+        alert("Your password cannot be blank");
+        return false;
+    }
+    else if(upsw.length < 6){
+        alert("Your Password must be greater than 6");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
+let mybutton = document.getElementById('top');
+		window.onscroll = function() {scrollFunction()};
+		
+		function scrollFunction() {
+		  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			mybutton.style.display = "block";
+		  } else {
+			mybutton.style.display = "none";
+		  }
+		}
+		function topFunction() {
+		  document.body.scrollTop = 0;
+		  document.documentElement.scrollTop = 0;
+		}
+function CheckOutValidate() {
+            var fname=document.checkOutForm.firstname.value;
+            var email=document.checkOutForm.email.value;
+            var atposition=email.indexOf("@");
+            var dotposition=email.lastIndexOf(".");
+            var uaddr=document.checkOutForm.address.value;
+            var ucity=document.checkOutForm.city.value;
+            var ustate=document.checkOutForm.state.value;
+            var uzip=document.checkOutForm.zip.value;
+            var x=document.checkOutForm.email.value;
+            if (fname == null || fname == "") {
+                alert("First Name cannot be blank");
+                return false;
+            }
+            else if(email == null || email == ""){
+                alert("Email cannot be blank");
+                return false;
+            }
+            else if(atposition<1 || dotposition<atposition+2 || dotposition+2 >= email.length){
+                alert("Please enter a valid email");
+                return false;
+            }
+            else if(uaddr == null || uaddr == ""){
+                alert("Your address cannot be blank");
+                return false;
+            }
+            else if(ucity == null || ucity == ""){
+                alert("Your city cannto be blank");
+                return false;
+            }
+            else if(ustate == null || ustate == ""){
+                alert("Your state cannot be blank");
+                return false;
+            }  
+            else if(uzip == null || uzip == ""){
+                alert("Please enter a zip code");
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
